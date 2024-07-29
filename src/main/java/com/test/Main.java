21f3a06a57c58b474d46dfc59127ba4c3e785d81
Jeong <jt26@naver.com>
2024-07-29 14:29:21 +0900

    ok

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -5,7 +5,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("have a good day");
         System.out.println("good day");
-        System.out.println("good day_hpe");
+        System.out.println("good day_hpe_test");
+        
 
     }
 }
